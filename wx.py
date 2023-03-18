4958cccca0e3d0bc1aef832d3df674f6197c3a7d
Cheng Huang <chhuang@kns.com>
2023-03-18 21:47:01 +0800

    wx get access token and user list

--- wx.py
+++ wx.py
@@ -1,4 +1,4 @@
-import hashlib,os
+import hashlib,os,requests,time
 
 from flask import Blueprint,abort,request,current_app
 
@@ -6,6 +6,10 @@ module_wx = Blueprint('module_wx', __name__, url_prefix='/wx')
 current_app.config['WX_TOKEN'] = os.getenv('WX_TOKEN')
 current_app.config['WX_ID'] = os.getenv('WX_ID')
 current_app.config['WX_PWD'] = os.getenv('WX_PWD')
+current_app.config['WX_ACCESS_TIME'] = 0
+current_app.config['WX_ACCESS_EXPIRE'] = 7000
+current_app.config['WX_USER_TOTAL'] = 0
+current_app.config['WX_USER_COUNT'] = 0
 
 @module_wx.route('/token')
 def token():
@@ -22,3 +26,46 @@ def token():
     else:
         return abort(403)
 
+@module_wx.route('/access_token')
+def access_token():
+    access_time = current_app.config['WX_ACCESS_TIME']
+    t = int(time.time())
+    print(access_time,t)
+    if access_time != 0 and t - access_time < current_app.config['WX_ACCESS_EXPIRE']:
+        print(current_app.config['WX_ACCESS_TOKEN'])
+        return 'OK'
+    
+    APPID = current_app.config['WX_ID']
+    APPSECRET = current_app.config['WX_PWD']
+    url = f'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={APPID}&secret={APPSECRET}'
+    print(url)
+    r = requests.get(url)
+    j = r.json()
+    if 'access_token' in j:
+        current_app.config['WX_ACCESS_TOKEN'] = j['access_token']
+        current_app.config['WX_ACCESS_TIME'] = t
+        print(current_app.config['WX_ACCESS_TOKEN'])
+        return 'OK'
+    else:
+        about(500)
+    
+#https://api.weixin.qq.com/cgi-bin/user/get?access_token=ACCESS_TOKEN&next_openid=NEXT_OPENID
+@module_wx.route('/user_list')
+def user_list():
+    access_token()
+    
+    ACCESS_TOKEN=current_app.config['WX_ACCESS_TOKEN']
+    url = f'https://api.weixin.qq.com/cgi-bin/user/get?access_token={ACCESS_TOKEN}'
+    print(url)
+    r = requests.get(url)
+    return r.text
+    j = r.json()
+    if 'total' in j and 'count' in j:
+        current_app.config['WX_USER_TOTAL'] = j['total']
+        current_app.config['WX_USER_COUNT'] = j['count']
+        if j['count'] > 0:
+            return j['data']
+        else:
+            return ''
+    else:
+      abort(500)       
